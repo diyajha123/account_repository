9b1f436f5215426c7230d2987ff06eed84ac21db
diyajha123 <122532436+diyajha123@users.noreply.github.com>
2023-05-10 11:21:33 +0530

    Update AccountRepositoryInterface.php

--- app/Interfaces/AccountRepositoryInterface.php
+++ app/Interfaces/AccountRepositoryInterface.php
@@ -6,9 +6,9 @@ interface AccountRepositoryInterface
     public function getAllAccount();
     public function getAccountById($id);
     public function deleteAccount($id);
-    public function CreateAccount(array $accounts);
-    public function updateAccount($account,array $newDetails);
-    public function getFulfilledAccounts();
+    public function CreateAccount( $accounts);
+    public function updateAccount($account,$newDetails);
+   
 
 
 }
